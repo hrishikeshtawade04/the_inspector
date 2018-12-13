76986bbc1c4c53a27a50ebd9a0da478f94a191d3
Hrishikesh Tawade <hrishikeshtawade@gmail.com>
2018-12-12 20:54:13 -0500

    [#9 : IROB] Added implementations of all methods for RobotModule Class

--- src/RobotModule.cpp
+++ src/RobotModule.cpp
@@ -1,8 +1,8 @@
 /**
  * BSD 3-Clause License
 
- 
- * Copyright (c) 2018, KapilRawal, Hrishikesh Tawde.
+
+ * Copyright (c) 2018, KapilRawal, Hrishikesh Tawade.
  * All rights reserved.
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -38,29 +38,104 @@
  *
  *  @copyright   BSD License
  *
- *  @brief    Robot file
+ *  @brief    Robot Class Implementation file
  *
  *  @section   DESCRIPTION
- * 
- *  Robot module calls the pathplanning and image processing class for execution of task
+ *
+ *  This file contains the implementations of the methods of Robot Class
  */
 
+#include "../include/RobotModule.h"
 #include <iostream>
 #include <vector>
 #include <string>
 #include "ros/ros.h"
-#include "RobotModule.h"
 
-Robot::Robot() {
-  leakageCount_ = 0;
+Robot::Robot()
+    : leakageCount_(0) {
 }
 
 Robot::~Robot() {
 }
 
-void Robot::findLeakages() {
+std::vector<std::vector<double>> Robot::putCoordinates(double wall13,
+                                                       double wall24) {
+  std::vector<std::vector<double>> vector2;
+  {
+    std::vector<double> vector1;
+    vector1.emplace_back(0);
+    vector1.emplace_back(-wall24);
+    vector1.emplace_back(90);
+    vector2.emplace_back(vector1);
+  }
+  {
+    std::vector<double> vector1;
+    vector1.emplace_back(wall13);
+    vector1.emplace_back(0);
+    vector1.emplace_back(180);
+    vector2.emplace_back(vector1);
+  }
+  {
+    std::vector<double> vector1;
+    vector1.emplace_back(0);
+    vector1.emplace_back(wall24);
+    vector1.emplace_back(-90);
+    vector2.emplace_back(vector1);
+  }
+  {
+    std::vector<double> vector1;
+    vector1.emplace_back(-wall13);
+    vector1.emplace_back(0);
+    vector1.emplace_back(0);
+    vector2.emplace_back(vector1);
+  }
+  return vector2;
+}
+std::vector<std::vector<double>> Robot::genClickCoordinates(double length,
+                                                            double breadth) {
+  /// finding the  coordinates based on turtlebot camera field of view
+
+    double wall13 = 0;
+    double wall24 = 0;
+    wall13 = breadth * 0.7875 - length / 2;
+    wall24 = length * 0.7875 - breadth / 2;
+    return putCoordinates(wall13, wall24);
 }
 
-std::vector<double> Robot::getLeakages() {
-return leakageLocations_;
+void Robot::findLeakages(std::vector<std::vector<double>> mapCoordinates) {
+  std::vector<double> locations;
+  int counter = 0;
+  double prevX = 0;
+  int wallCount = 1;
+  ros::Rate rate(50.0);
+  /// go to the  coordinates, click images and find leakage locations
+  /// with respect to wall and then print
+  for (std::vector<double> pos : mapCoordinates) {
+    pathPlanner_.setGoal(pos[0], pos[1]);
+  ROS_INFO("Goal Set");
+  pathPlanner_.navigate();
+    pathPlanner_.moveToAngle(pos[2]);
+  ros::Duration(3.0).sleep();
+  ROS_INFO("Navigation Completed");
+  leakageLocations_ = imageProcessor_.detectContours(wallcount,1)
+  }
+  ROS_INFO_STREAM("Total Leakages = " << leakageCount_);
+  for (auto leakage : leakageLocations_) {
+    if (counter % 16 == 0) {
+      ROS_INFO_STREAM("Wall " << wallCount << "Leakages");
+      ++wallCount;
+    }
+    if (counter % 2 == 0) {
+      prevX = leakage;
+    } else {
+      ROS_INFO_STREAM("X = " << prevX << ", Y = " << leakage);
+    }
+    ++counter;
+  }
+  ROS_INFO("Leakge finding complete");
 }
+
+int Robot::getLeakageCount() {
+  return leakageCount_;
+}
+
