d1f5fdadfa75803c8dae84f8bc84a77cd552beca
Hrishikesh Tawade <hrishikeshtawade@gmail.com>
2018-12-12 21:26:22 -0500

    [#1 : CP] Updated main.cpp file running the turtlebot to find leakages

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
  * BSD 3-Clause License
 
 
- * Copyright (c) 2018, KapilRawal, Hrishikesh Tawde.
+ * Copyright (c) 2018, KapilRawal, Hrishikesh Tawade
  * All rights reserved.
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -38,29 +38,31 @@
  *
  *  @copyright   BSD License
  *
- *  @brief    main file
+ *  @brief   ROS the_inspector node
  *
  *  @section   DESCRIPTION
  *
- *  Main file for the the_inspector package which calls the Robot module.
+ *  This function tells the Turtlebot to find leakages
+ *
  */
 
 #include "RobotModule.h"
+#include <ros/ros.h>
+#include <iostream>
 
-/**
- *  @brief   This tutorial demonstrates simple Receving of messages over the ROS system.
- *
- *  @param   argc  Count of arguments given on terminal
- *
- *  @param   argv  Pointer to the array of pointers pointing to the argument given in the terminal
- *
- *  @return  returns nothing
- */
-int main(int argc, char **argv) {
 
+int main(int argc, char** argv) {
+  /// Initializing the node
   ros::init(argc, argv, "the_inspector");
+  /// Making instance of robot
   Robot turtleBot;
-  turtleBot.findLeakages();
-
- return 0;
+  /// getting mapCoordinates for robot to travel
+  std::vector<std::vector<double>> mapCoordinates = turtleBot
+      .genClickCoordinates(8, 8);
+  /// find leakages only when there are map coordinates otherwise don't
+  turtleBot.findLeakages(mapCoordinates);
+  /// shutdown the node
+  ros::shutdown();
+  return 0;
 }
+
